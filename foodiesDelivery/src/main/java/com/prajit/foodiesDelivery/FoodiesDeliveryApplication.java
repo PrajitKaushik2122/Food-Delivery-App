7917c3aeeb7b51dea29fe65f3b00aaa9487f50d8
Prajit <prajitkaushik21b@gmail.com>
2025-11-02 21:31:49 +0530

    project setup and aws config

---
+++ foodiesDelivery/src/main/java/com/prajit/foodiesDelivery/FoodiesDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.prajit.foodiesDelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodiesDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodiesDeliveryApplication.class, args);
+	}
+
+}
---
+++ foodiesDelivery/src/test/java/com/prajit/foodiesDelivery/FoodiesDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prajit.foodiesDelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodiesDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
